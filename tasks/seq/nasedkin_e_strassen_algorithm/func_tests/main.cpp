38af81385b138be7178bd97068c9574edb1bc5bd
myffii <132840123+myffii@users.noreply.github.com>
2025-02-15 13:49:48 +0300

    1

--- tasks/seq/nasedkin_e_strassen_algorithm/func_tests/main.cpp
+++ tasks/seq/nasedkin_e_strassen_algorithm/func_tests/main.cpp
@@ -54,38 +54,3 @@ TEST(nasedkin_e_strassen_algorithm_seq, test_matmul_50) {
   // Проверка результата (опционально, если есть ожидаемый результат)
   // EXPECT_EQ(expected_result, out);
 }
-
-TEST(nasedkin_e_strassen_algorithm_seq, test_matmul_100_from_file) {
-  std::string line;
-  std::ifstream test_file(ppc::util::GetAbsolutePath("seq/nasedkin_e_strassen_algorithm/data/test.txt"));
-  if (test_file.is_open()) {
-    getline(test_file, line);
-  }
-  test_file.close();
-
-  const size_t count = std::stoul(line);
-
-  // Генерация случайных матриц
-  std::vector<int> in_a = GenerateRandomMatrix(count);
-  std::vector<int> in_b = GenerateRandomMatrix(count);
-  std::vector<int> out(count * count, 0);
-
-  // Create task_data
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_a.data()));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_b.data()));
-  task_data_seq->inputs_count.emplace_back(in_a.size());
-  task_data_seq->inputs_count.emplace_back(in_b.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-  task_data_seq->outputs_count.emplace_back(out.size());
-
-  // Create Task
-  nasedkin_e_strassen_algorithm_seq::StrassenSequential test_task_sequential(task_data_seq);
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
-  // Проверка результата (опционально, если есть ожидаемый результат)
-  // EXPECT_EQ(expected_result, out);
-}
