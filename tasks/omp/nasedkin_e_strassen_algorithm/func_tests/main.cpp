14637cdcea4b68bb19b919afc2bfe7678eae4982
myffii <132840123+myffii@users.noreply.github.com>
2025-03-22 06:47:25 +0300

    Remove cout

--- tasks/omp/nasedkin_e_strassen_algorithm/func_tests/main.cpp
+++ tasks/omp/nasedkin_e_strassen_algorithm/func_tests/main.cpp
@@ -70,7 +70,6 @@ void RunFixedMatrixTest(int size) {
   for (int i = 0; i < static_cast<int>(expected.size()); ++i) {
     EXPECT_NEAR(expected[i], out[i], 1e-6);
   }
-  std::cout << "fixed matrix test: success" << "\n";
 }
 }  // namespace
 
