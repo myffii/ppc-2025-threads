dab9ffe5b99fcbe12233009e166bf10c45de3baf
myffii <132840123+myffii@users.noreply.github.com>
2025-05-06 16:40:15 +0300

    1

--- tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
+++ tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
@@ -1,3 +1,5 @@
+#include "all/nasedkin_e_strassen_algorithm/include/ops_all.hpp"
+
 #include <algorithm>
 #include <boost/mpi.hpp>
 #include <boost/serialization/vector.hpp>
@@ -6,7 +8,6 @@
 #include <thread>
 #include <vector>
 
-#include "all/nasedkin_e_strassen_algorithm/include/ops_all.hpp"
 #include "core/util/include/util.hpp"
 
 namespace nasedkin_e_strassen_algorithm_all {
