8d8176fd9b108c18d3d5bc9b1e8160f74edb17ad
myffii <132840123+myffii@users.noreply.github.com>
2025-05-08 19:02:39 +0300

    1

--- tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
+++ tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
@@ -4,6 +4,7 @@
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/collectives/gather.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/serialization/vector.hpp>
 #include <cmath>
 #include <cstddef>
 #include <functional>
