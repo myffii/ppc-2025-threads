d7370db472cdc19c26921269dbfa5d3b313595b3
myffii <132840123+myffii@users.noreply.github.com>
2025-05-08 16:29:24 +0300

    1

--- tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
+++ tasks/all/nasedkin_e_strassen_algorithm/src/ops_all.cpp
@@ -1,3 +1,5 @@
+#include "all/nasedkin_e_strassen_algorithm/include/ops_all.hpp"
+
 #include <algorithm>
 #include <boost/mpi.hpp>
 #include <boost/serialization/vector.hpp>
@@ -7,7 +9,6 @@
 #include <thread>
 #include <vector>
 
-#include "all/nasedkin_e_strassen_algorithm/include/ops_all.hpp"
 #include "core/util/include/util.hpp"
 
 namespace mpi = boost::mpi;
